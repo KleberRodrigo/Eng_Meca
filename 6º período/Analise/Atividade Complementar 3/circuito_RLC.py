ad14bc1ff71df4e26e8139f156c64bff4bb9b5af
Kleber Junior <kleberjunior1999@gmail.com>
2022-09-05 23:49:08 -0300

    editado

--- 6º período/Analise/Atividade Complementar 3/circuito_RLC.py
+++ 6º período/Analise/Atividade Complementar 3/circuito_RLC.py
@@ -10,12 +10,18 @@ import math as m
 
 plt.close('all')
 
+def didt(T):
+    return (  (m.exp(-0.125*T)) * (((-0.3307*m.sin(0.3307*T))) - (0.125*m.cos(0.3307*T))) )  
+
 #Variáveis de simulação 
 T  = 0 #tempo real 
 Ti = 0 #segundos
 Tf = 100 #segundos
 step = 0.01 #segundos
 
+#Variaveis do sistema 
+
+I=1 # corrente inicial
 
 #Eixos de plotagem
 I_eixo = []
@@ -25,11 +31,14 @@ T_eixo = []
 while(T<=Tf):
     #Preenche os Vetores
     I_eixo.append(I)
-    V_eixo.append(V)
+    #V_eixo.append(V)
     T_eixo.append(T)
 
     #Armazena o valor Numérico 
-    I = ((m.exp(-0.125 * T)*m.cos(0.3307))+(1.1339*(m.exp(-0.125*T)*m.sin(0.3307))))
+    #I = ((m.exp(-0.125 * T)*m.cos(0.3307))+(1.1339*(m.exp(-0.125*T)*m.sin(0.3307))))
+    #Solução da EDO:
+    sol = solve_ivp(didt, t_span=(T, T+step), y0=[I], method='RK45', t_eval=[T, T+step])
+    I = sol.y[0][-1]
     #Tempo instantânio
     T += (Ti + step)
 
